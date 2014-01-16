1bd041f25ba2ad9aa4e45a5053f73cf758d25d12
Chrissy Wainwright <cwainwright@gmail.com>
2014-01-16 16:45:04 -0500

    hide footer from presenter display, improve inline code styles

--- js/hovercraft.js
+++ js/hovercraft.js
@@ -3,10 +3,12 @@ impress().init();
 
 // Set up the help-box
 var helpdiv = window.document.getElementById('hovercraft-help');
+var footer = window.document.getElementById('footer');
 
 if (window.top!=window.self) {
     // This is inside an iframe, so don't show the help.
     helpdiv.className = "disabled";
+    footer.className = "disabled";
     
 } else {
     // Install a funtion to toggle help on and off.
